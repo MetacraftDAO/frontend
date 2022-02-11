952a9286ee567e5bc00d33a6701a65a758369445
Sharu <jsrshark110@gmail.com>
2022-02-10 21:28:24 -0800

    Add gas and deposit

--- src/Components/Mint/index.tsx
+++ src/Components/Mint/index.tsx
@@ -1,5 +1,6 @@
 import {nearWallet} from "../wallet";
-import * as nearAPI from "near-api-js"
+import * as nearAPI from "near-api-js";
+const BN = require('bn.js');
 
 const Mint = () => {
 
@@ -10,7 +11,7 @@ const Mint = () => {
         console.log("mint presseed" + wallet.getAccountId());
         const contract = await new nearAPI.Contract(
             account,
-            "dev-1644547440196-90831848776425",
+            "katesonia5.testnet",
             {
                 // name of contract you're connecting to
                 viewMethods: ["getMessages", "nft_metadata"], // view methods do not change state but usually return a value
@@ -34,7 +35,9 @@ const Mint = () => {
                     "media": "https://cdn.myminifactory.com/assets/object-assets/5dcff80379a67/images/720X720-schuin.jpg",
                     "copies": 1
                 }
-            }
+            },
+            new BN('26B4BD9110D0', 16),
+            new BN('26B4BD9110DCE800000', 16)
         );
         console.log(response);
     }

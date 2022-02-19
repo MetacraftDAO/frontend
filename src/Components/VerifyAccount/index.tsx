a9ea92a1d72acfaf5dcd5001518987da4c0a4001
Sharu <jsrshark110@gmail.com>
2022-02-18 20:31:19 -0800

    Update verify logic

--- src/Components/VerifyAccount/index.tsx
+++ src/Components/VerifyAccount/index.tsx
@@ -9,7 +9,7 @@ import Parse from 'parse/dist/parse.min.js';
 Parse.initialize(process.env.REACT_APP_APPLICATION_ID, process.env.REACT_APP_JAVASCRIPT_KEY);
 Parse.serverURL = process.env.REACT_APP_HOST_URL;
 
-const verify = async (uuid: string, username: string) => {
+const connectedWalletAndHasNft = async () => {
   if(!nearWallet.isSignedIn()) return false;
   //@ts-ignore
   const nfts = await nftContract.nft_tokens_for_owner(
@@ -17,33 +17,77 @@ const verify = async (uuid: string, username: string) => {
         "account_id": nearWallet.getAccountId()
     }
   );
-  if (nfts.length == 0) return false;
-  let verified = await fetchVerifiedUser(uuid);
-  if (verified && verified.get('isVerified') && verified.get('nearAccountId') == nearWallet.getAccountId()) return true;
+  return nfts.length > 0;
+}
 
-  if (verified && verified.get('nearAccountId') != nearWallet.getAccountId()) {
-    verified.set("nearAccountId", nearWallet.getAccountId());
-    verified.set("isVerified", true);
-    await verified.save();
-    return true;
-  }
-
+const writeNewVerifiedUserToDB = async (uuid: string, username: string, nearAccountId: string, isVerified: boolean) => {
   const User = new Parse.Object("VerifiedUser");
   User.set("uuid", uuid);
   User.set("username", username);
-  User.set("nearAccountId", nearWallet.getAccountId());
-  User.set("isVerified", true);
+  User.set("nearAccountId", nearAccountId);
+  User.set("isVerified", isVerified);
   await User.save();
-  return true;
+}
+
+const updateExistingVerifiedUserToDB = async (User: any, uuid: string, username: string, nearAccountId: string, isVerified: boolean) => {
+  User.set("uuid", uuid);
+  User.set("username", username);
+  User.set("nearAccountId", nearAccountId);
+  User.set("isVerified", isVerified);
+  await User.save();
+}
+
+const verify = async (uuid: string | null, username: string | null) => {
+  // If wallet not connected or have no nft, return false.
+  let connected = await connectedWalletAndHasNft();
+  if (!connected) return [username, false];
+  let nearAccountId = nearWallet.getAccountId();
+
+  let verifiedUser = await fetchVerifiedUserByNearAccountId(nearAccountId);
+  // If no existing DB object for near account id.
+  if (!verifiedUser) {
+    if(!uuid || !username) {
+      return [null, false];
+    }
+
+    let existingUser = await fetchVerifiedUserByUUID(uuid);
+    if (!existingUser) {
+      await writeNewVerifiedUserToDB(uuid, username, nearAccountId, true);
+    } else {
+      await updateExistingVerifiedUserToDB(existingUser, uuid, username, nearAccountId, true);
+    }
+    return [username, true];
+  }
+
+  // If there is a existing DB object for near account id, already verified and the near account is the same as current account
+  // No need to update the DB.
+  if (verifiedUser.get('isVerified') && (verifiedUser.get('username') == username || !username)) {
+    return [verifiedUser.get('username'), true];
+  }
+
+  await updateExistingVerifiedUserToDB(
+    verifiedUser, uuid ? uuid : verifiedUser.get('uuid'), 
+    username ? username : verifiedUser.get('username'), 
+    nearAccountId, true);
+  return [username ? username : verifiedUser.get('username'), true];
 }
 
 const displayMsg = (isVerified: boolean)=> {
   return isVerified ? "Account is successfully verified!" : "Pls login with your wallet and verify";
 }
 
+const fetchVerifiedUserByNearAccountId = async (nearAccountId: string) => {
+  const query = new Parse.Query('VerifiedUser');
+  // use the equalTo filter to look for user which the name is John. this filter can be used in any data type
+  query.equalTo('nearAccountId', nearAccountId);
+  // run the query
+  const results = await query.findAll();
+  // @ts-ignore
+  console.log('results ', results);
+  return results.length > 0 ? results[0] : null;
+}
 
-
-const fetchVerifiedUser = async (uuid: string) => {
+const fetchVerifiedUserByUUID = async (uuid: string) => {
   const query = new Parse.Query('VerifiedUser');
   // use the equalTo filter to look for user which the name is John. this filter can be used in any data type
   query.equalTo('uuid', uuid);
@@ -54,9 +98,7 @@ const fetchVerifiedUser = async (uuid: string) => {
   return results.length > 0 ? results[0] : null;
 }
 
-const VerifyAccount = () => {
-  const [isVerified, setIsVerified] = useState(false);
-
+const getUUIDandUsername = ()=>{
   let queryString = window.location.search;
   let urlParams = new URLSearchParams(queryString);
   let encoded = urlParams.get('params');
@@ -65,19 +107,25 @@ const VerifyAccount = () => {
   urlParams = new URLSearchParams(decoded);
   let uuid = urlParams.get("uuid");
   let username = urlParams.get("username");
+  return {uuid, username};
+}
 
-
-  const setVerifyStatus = async () => {
-    if (uuid) {
-      verify(uuid ? uuid : "", username ? username : "").then((verified: boolean) => {
-        setIsVerified(verified);
-      });
-    }
+const VerifyAccount = () => {
+  const [isVerified, setIsVerified] = useState(false);
+  let {uuid, username} = getUUIDandUsername();
+  const [linkedUsername, setlinkedUsername] = useState(username);
+  
+  const setVerifyStatus = () => {
+    //@ts-ignore
+    verify(uuid, username).then(([username, isVerified]) => {
+      setlinkedUsername(username);
+      setIsVerified(isVerified);
+    });
   }
 
   return (
     <>
-    {!username ? (<h1>No minecraft user to verify!<br></br>Enter minecraft and type /verify</h1>) : (<h1>{"Minecraft user " + username}</h1>)}
+    {!linkedUsername ? (<h1>No minecraft user to verify!<br></br>Enter minecraft and type /verify</h1>) : (<h1>{"Minecraft user " + linkedUsername}</h1>)}
     {isVerified && <h1>{displayMsg(isVerified)}</h1>}
     <button onClick={setVerifyStatus}> Verify account </button>
     </>

dfe07bbda216de2e69a93cba73665389c8dc7764
yongli-current <yongli@current.com>
2022-02-24 13:34:23 -0500

    social icons

--- src/Components/Footer/index.tsx
+++ src/Components/Footer/index.tsx
@@ -10,11 +10,14 @@ const Footer = () => {
         <FooterSection>
             <Container>
                 <Socials>
-                    <Link target="_blank" rel="noopener noreferrer" href={"https://twitter.com/Amoebafriends"} >
+                    <Link target="_blank" rel="noopener noreferrer" href={"https://twitter.com/MetacraftDAO"} >
                         <Img src="/img/twitter.png" alt="twitter"/>
                     </Link>
-                    <Link target="_blank" rel="noopener noreferrer" href={"https://discord.gg/amoebafriends"}>
-                        <Img  src="/img/discord.png" alt="discord"/>
+                    <Link target="_blank" rel="noopener noreferrer" href={"https://youtu.be/-yTRfY-PJBU"}>
+                        <Img  src="/img/youtube.png" alt="youtube"/>
+                    </Link>
+                    <Link target="_blank" rel="noopener noreferrer" href={"https://t.me/MetacraftDAO"}>
+                        <Img  src="/img/telegram.png" alt="youtube"/>
                     </Link>
                 </Socials>
             </Container>
